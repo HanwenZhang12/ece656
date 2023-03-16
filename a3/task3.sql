f9763f9129501c9d5c25ef393348cbf4f9d37931
belancynod <belancynod@gmail.com>
2023-03-16 17:34:49 -0400

    1

--- a3/task3.sql
+++ a3/task3.sql
@@ -1,21 +1,12 @@
-drop table if exists PeriodData;
-drop table if exists GamePlays;
-
-create table GamePlays (
-	playNumber varchar(45),
-    gameID varchar(20),
-	periodTime int not null,
-	periodTimeRemaining int not null,
-    dateTime datetime not null,
-    description varchar(100) not null,
-    primary key (playNumber),
-	foreign key (gameID) references Game (gameID)
-);
+alter table GamePlays 
+drop column periodTimeRemaining,
+drop column periodTime,
+drop column periodType,
+change column period periodNumber VARCHAR(10) NOT NULL;
 
 create table PeriodData (
 	periodNumber varchar(10),
     gameType varchar(10),
     periodType varchar(20) not null,
-    primary key (periodNumber, gameType),
-	foreign key (gameType) references Game (gameType)
+    primary key (periodNumber, gameType)
 );
