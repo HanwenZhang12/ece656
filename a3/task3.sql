4b5094fdba54fdd03ea8db2d93972a84e5b24578
belancynod <belancynod@gmail.com>
2023-03-17 11:50:00 -0400

    1

--- a3/task3.sql
+++ a3/task3.sql
@@ -1,6 +1,4 @@
-alter table GamePlays 
-drop column periodType,
-change column period periodNumber VARCHAR(10) NOT NULL;
+drop table if exists PeriodData;
 
 create table PeriodData (
 	periodNumber varchar(10),
@@ -8,3 +6,7 @@ create table PeriodData (
     periodType varchar(20) not null,
     primary key (periodNumber, gameType)
 );
+
+INSERT INTO PeriodData (periodNumber, gameType, periodType) SELECT distinct gp.period, g.gameType, gp.periodType FROM GamePlays gp INNER JOIN Game g ON gp.gameID = g.gameID;
+
+alter table GamePlays drop column periodType, change column period periodNumber VARCHAR(10) NOT NULL, add foreign key (period) references PeriodData(periodNumber);
